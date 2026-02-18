e4324ccc52a004321af9650b2e2942bb6d99c70b
flanker0507 <108620970+flanker0507@users.noreply.github.com>
2026-02-18 15:57:51 +0700

    formatter

--- js/script.js
+++
@@ -1,7 +0,0 @@
-// Toggle class active
-let navbarNav = document.querySelector(".navbar-nav");
-
-// ketika hamburger menu di klik
-document.querySelector("#menu").onclick = () => {
-  navbarNav.classList.toggle("active");
-};
