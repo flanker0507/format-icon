74fdafd2cceafe4ba4b58ccc7711469bd374fbdd
flanker0507 <108620970+flanker0507@users.noreply.github.com>
2026-02-19 15:30:28 +0700

    toggle

--- js/script.js
+++ js/script.js
@@ -5,3 +5,12 @@ let navbarNav = document.querySelector(".navbar-nav");
 document.querySelector("#menu").onclick = () => {
   navbarNav.classList.toggle("active");
 };
+
+// klik di luar sidebar untuk menghilangkan navbar
+let hamburger = document.querySelector("#menu");
+
+document.addEventListener("click", function (e) {
+  if (!hamburger.contains(e.target) && !navbarNav.contains(e.target)) {
+    navbarNav.classList.remove("active");
+  }
+});
